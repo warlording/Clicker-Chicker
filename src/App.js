c882c895222b54f03441e7f8efe2ebbc46c1d8a5
warlording <warlording@gmail.com>
2018-03-25 22:30:42 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,36 +1,127 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import MatchCard from "./components/MatchCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import matches from "./matchcards.json";
+import "./App.css";
 
-import ImageCard from './components/ImageCard';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
+let correctGuesses = 0;
+let bestScore = 0;
+let clickMessage = "Click on all 12 colors only once and you get a prize.";
 
 class App extends Component {
-  
-  state={
-    counter: 0,
-    topScore: 0
-  };
+    
+    // Setting this.state.matches to the matches json array
+    state = {
+        matches,
+        correctGuesses,
+        bestScore,
+        clickMessage
+    };
 
-  scoresToParent = (counter, topScore) => {
-    this.setState({
-      counter: counter,
-      topScore: topScore
-    })
-  };
+    setClicked = id => {
 
-  render() {
-    return (
-    <div>
-      <Navbar 
-        counter= {this.state.counter}
-        topScore= {this.state.topScore}
-      />
-      <Header />
-      <ImageCard parentCallback={this.scoresToParent} />
-    </div>
-    );
-  }
+        // Make a copy of the state matches array to work with
+        const matches = this.state.matches;
+
+        // Filter for the clicked match
+        const clickedMatch = matches.filter(match => match.id === id);
+
+        // If the matched image's clicked value is already true, 
+        // do the game over actions
+        if (clickedMatch[0].clicked){
+
+            console.log ("Correct Guesses: " + correctGuesses);
+            console.log ("Best Score: " + bestScore);
+
+            correctGuesses = 0;
+            clickMessage = "You Goofed. Start over."
+
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            this.setState({clickMessage});
+            this.setState({ correctGuesses });
+            this.setState({matches});
+
+        // Otherwise, if clicked = false, and the user hasn't finished
+        } else if (correctGuesses < 11) {
+
+            // Set its value to true
+            clickedMatch[0].clicked = true;
+
+            // increment the appropriate counter
+            correctGuesses++;
+            
+            clickMessage = "Lucky guess. Do it again.";
+
+            if (correctGuesses > bestScore){
+                bestScore = correctGuesses;
+                this.setState({ bestScore });
+            }
+
+            // Shuffle the array to be rendered in a random order
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            // Set this.state.matches equal to the new matches array
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+        } else {
+
+            // Set its value to true
+            clickedMatch[0].clicked = true;
+
+            // restart the guess counter
+            correctGuesses = 0;
+
+            // Egg on the user to play again
+            clickMessage = "You are either really lucky, or autistic. You win.";
+            bestScore = 12;
+            this.setState({ bestScore });
+            
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            // Shuffle the array to be rendered in a random order
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            // Set this.state.matches equal to the new matches array
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+
+        }
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Title>Hope you're not color-blind!</Title>
+        
+                <h3 className="scoreSummary">
+                    {this.state.clickMessage}
+                </h3>
+                
+                <h3 className="scoreSummary">
+                    Correct Guesses: {this.state.correctGuesses} 
+                    <br />
+                    Best Score: {this.state.bestScore} 
+                </h3>
+
+                {this.state.matches.map(match => (
+                    <MatchCard
+                        setClicked={this.setClicked}
+                        id={match.id}
+                        key={match.id}
+                        image={match.image}
+                    />
+                ))}
+            </Wrapper>
+        );
+    }
 }
 
 export default App;
--- src/components/Header/Header.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Header = props => 
-
-	<header className="App-header">
-	  <h1 className="App-title">Tanner's Color Picker</h1>
-	</header>
-export default Header;
